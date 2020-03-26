76062d4fae44d7c1eb018bfa40472c12cf359fee
Alexandra Zakharova <alexandra.v.zakharova@gmail.com>
2020-03-26 21:35:23 +0300

    change send method

--- routes/wrongPage.js
+++ routes/wrongPage.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 
 router.all('/', (req, res) => {
-  res.status(404).send(JSON.stringify({ message: 'Запрашиваемый ресурс не найден' }));
+  res.status(404).json({ message: 'Запрашиваемый ресурс не найден' });
 });
 
 module.exports = router;
