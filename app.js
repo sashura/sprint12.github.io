46894106856a14725aa6c36e93646fb7e291458c
Alexandra Zakharova <alexandra.v.zakharova@gmail.com>
2020-04-01 12:37:07 +0300

    connect to mongoose, use bodyparser

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const path = require('path');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 const users = require('./routes/users');
 const cards = require('./routes/cards');
@@ -10,8 +12,23 @@ const { PORT = 3000 } = process.env;
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'public')));
+mongoose.connect('mongodb://localhost:27017/mestodb', {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true,
+});
 
+app.use(bodyParser.json());
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use((req, res, next) => {
+  req.user = {
+    _id: '5e82f452f6a5d4b2fe2b1c5a',
+  };
+
+  next();
+});
 
 app.use('/cards', cards);
 app.use('/users', users);
