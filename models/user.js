f59c23075ff09ee86cda3f035046989c92fb349f
Alexandra Zakharova <alexandra.v.zakharova@gmail.com>
2020-04-07 20:53:26 +0300

    fix eslint problems

--- models/user.js
+++ models/user.js
@@ -37,7 +37,7 @@ const userSchema = new mongoose.Schema({
   },
 });
 
-userSchema.statics.findUserByCredentials = function (email, password) {
+userSchema.statics.findUserByCredentials = function findByAuthParamets(email, password) {
   return this.findOne({ email }).select('+password')
     .then((user) => {
       if (!user) {
