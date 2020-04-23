5fe6aa3effc91136ae9ae28974b2379acd0e771a
Alexandra Zakharova <alexandra.v.zakharova@gmail.com>
2020-04-23 20:58:06 +0300

    change joi password validation

--- app.js
+++ app.js
@@ -41,7 +41,7 @@ app.get('/crash-test', () => {
 app.post('/signin', celebrate({
   body: Joi.object().keys({
     email: Joi.string().required().email(),
-    password: Joi.string().required(),
+    password: Joi.string().required().min(5),
   }),
 }), login);
 
