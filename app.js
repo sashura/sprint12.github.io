5b2157bd4e8210aa69f8c66252a654d221f68e8b
Alexandra Zakharova <alexandra.v.zakharova@gmail.com>
2020-04-24 14:59:28 +0300

    try to crash-test

--- app.js
+++ app.js
@@ -32,11 +32,11 @@ app.use(bodyParser.json());
 
 app.use(requestLogger);
 
-app.get('/crash-test', () => {
-  setTimeout(() => {
-    throw new Error('Сервер сейчас упадёт');
-  }, 0);
-});
+// app.get('/crash-test', () => {
+// setTimeout(() => {
+// throw new Error('Сервер сейчас упадёт');
+// }, 0);
+// });
 
 app.post('/signin', celebrate({
   body: Joi.object().keys({
